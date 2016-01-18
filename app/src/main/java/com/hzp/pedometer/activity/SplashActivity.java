2b70c7a5f7f0e8207b0df9075cf278aedaf77e24
hezhipeng <hoholiday@hotmail.com>
2016-01-18 22:15:24 +0800

    完善闪屏界面逻辑

--- app/src/main/java/com/hzp/pedometer/activity/SplashActivity.java
+++ app/src/main/java/com/hzp/pedometer/activity/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.hzp.pedometer.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,9 +14,28 @@ import com.hzp.pedometer.R;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private long waitTime = 1000;//闪屏等待时间
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+    }
+
+    /**
+     * 开启启动主界面的任务
+     */
+    private void startMainActivity(){
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: 2016/1/18 完成主界面跳转
+                Intent intent = new Intent(SplashActivity.this,);
+                startActivity(intent);
+            }
+        },waitTime);
     }
 }
