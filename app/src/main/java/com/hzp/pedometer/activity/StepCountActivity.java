1802bdddf2bba4ea6aa5658a3fd0b20bc3d9e15a
hezhipeng <hoholiday@hotmail.com>
2016-01-20 20:09:51 +0800

    更新仪表盘ui

--- app/src/main/java/com/hzp/pedometer/activity/StepCountActivity.java
+++ app/src/main/java/com/hzp/pedometer/activity/StepCountActivity.java
@@ -2,17 +2,36 @@ package com.hzp.pedometer.activity;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 
 import com.hzp.pedometer.R;
+import com.hzp.pedometer.components.RateDashboard;
 
 /**
  * 计步器工作页面
  */
 public class StepCountActivity extends BindingActivity {
 
+    private RateDashboard rateDashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_count);
+
+
+        initViews();
+    }
+
+    private void initViews(){
+        rateDashboard = (RateDashboard) findViewById(R.id.step_rate_dashboard_view);
+        rateDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rateDashboard.setValue();
+            }
+        });
     }
 }
--- app/src/main/java/com/hzp/pedometer/components/RateDashboard.java
+++ app/src/main/java/com/hzp/pedometer/components/RateDashboard.java
@@ -1,19 +1,27 @@
 package com.hzp.pedometer.components;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.hzp.pedometer.R;
+
+import java.util.Random;
 
 /**
  * @author 何志鹏 on 2016/1/19.
  * @email hoholiday@hotmail.com
  */
 public class RateDashboard extends FrameLayout{
-    private ImageView dashArrow;
+    private ImageView pointer;
+    private Animation rotateAnimation;
+
 
     public RateDashboard(Context context) {
         this(context, null);
@@ -25,11 +33,30 @@ public class RateDashboard extends FrameLayout{
 
     public RateDashboard(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        LayoutInflater.from(context).inflate(R.layout.ui_rate_dashboard, this);
+        pointer = (ImageView) findViewById(R.id.rate_dashboard_pointer);
 
-        LayoutInflater.from(context).inflate(R.layout.ui_rate_dashboard,this);
     }
-
+int temp1 =-120,temp2 =0;
     public void setValue(){
+
+        temp2 = new Random().nextInt(120);
+        switch (new Random().nextInt(2)){
+            case 0:
+                break;
+            case 1:
+                temp2 = -temp2;
+                break;
+        }
+        rotateAnimation = new
+                RotateAnimation(temp1,temp2
+                ,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        temp1 = temp2;
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setFillEnabled(true);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setRepeatCount(0);
+        pointer.startAnimation(rotateAnimation);
 
     }
 
